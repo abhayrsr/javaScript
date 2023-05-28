d3acc7053cdb337899c92e22d3342e4f3910a22f
abhay <abhayrajsinghrana123@gmail.com>
2023-05-28 17:10:23 +0530

    feat: use of this

---
+++ Objects/this.js
@@ -0,0 +1,16 @@
+const user1 ={
+    name: "Abhay",
+    age: 24,
+    greet: function(){
+        console.log(this.age);
+    }
+}
+const user2 ={
+    name: "Tarun",
+    age: 24,
+    }
+
+user2.greet = user1.greet;
+user1.greet();
+user2.greet();
+/* arrow function's this refers to window object*/
