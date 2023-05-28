3748a1e690d40ee4d8a9d90532f7b9a268e5868b
abhay <abhayrajsinghrana123@gmail.com>
2023-05-28 10:38:31 +0530

    feat: function as a property

---
+++ Objects/functionProperty.js
@@ -0,0 +1,17 @@
+const func = {
+    
+    information: function() {
+        console.log("Function");
+    },
+
+    name(){
+        console.log("My name is Abhay");
+    }
+};
+
+func.information();
+func.name();
+
+/* two way to create a function as a property in an object.
+1. anonymous function
+2. function  by name */
