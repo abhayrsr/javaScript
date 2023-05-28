4dd431d3033b5acdb00c45662e8f499c0fcceb22
abhay <abhayrajsinghrana123@gmail.com>
2023-05-28 10:19:24 +0530

    refactor: added a comment

--- Objects/objectProperties.js
+++ Objects/objectProperties.js
@@ -9,3 +9,7 @@ const person = {
 
 person.information();
 console.log(person.name);
+
+/* keys in an object by default are strings and we can declare functions as a value in an object.
+Object literal is a clear and concise syntax to create an object. It allows us to define properties and expressions
+in a single line */
