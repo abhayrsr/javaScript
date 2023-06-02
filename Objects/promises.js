25ea41431ddd7a737be63e56643cd8658daaa7c0
abhay <abhayrajsinghrana123@gmail.com>
2023-06-02 14:52:16 +0530

    refactor: definition

--- Objects/promises.js
+++ Objects/promises.js
@@ -13,3 +13,7 @@ learnJs
 .catch((error) => {console.log(error)})
 
 .finally(() => {console.log("the promise is completed")})
+
+/*promise is an object which is used when a user needs an output in near future but not in the present. This concept was
+mainly introduced to counter the callback hell demerit of javascript, due to which the readability of the code was getting
+affected while executing the task asynchronously. Promise overcame this problem and maintained code readbility. */ 
