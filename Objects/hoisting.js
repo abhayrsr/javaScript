a96909496069a65940684a9230a58104639edc51
abhay <abhayrajsinghrana123@gmail.com>
2023-06-02 18:29:46 +0530

    feat: hoisting

---
+++ Objects/hoisting.js
@@ -0,0 +1,6 @@
+console.log(a);
+var a  = "Abhay";
+console.log(a);
+
+/* hoisting is a behaviour in js where the variable and functions declaration are put at the top of their scope 
+before the code is executed*/
