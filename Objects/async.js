8a4a4264d177beac5dd77a816d86c8f882f299ed
abhay <abhayrajsinghrana123@gmail.com>
2023-06-02 16:23:51 +0530

    refactor: comment

--- Objects/async.js
+++ Objects/async.js
@@ -20,7 +20,8 @@ getProfile.then((profile) => {
 })
 
 /* in order to overcome the nesting problem and increase the readbility of code, the concept of async/await was introduced.
-Async makes the asynchronous code seems to be executed in sequential manner and more readable */
+Async makes the asynchronous code seems to be executed in sequential manner and more readable. Async is used declare
+a asynchronous function. When a function is marked by async it means it will return a promise */
 
 async function getFemaleFriend(){
     const profile = await getProfile;
@@ -32,4 +33,5 @@ async function getFemaleFriend(){
 getFemaleFriend().then((femaleFriends) => {
     console.log(femaleFriends);
 })
-/* await can only be used with async */
+/* await can only be used with async. it pause the execution of code and wait for promise to resolve. If the promise is
+rejected, an exception is thrown which can be handled by try-catch block*/
